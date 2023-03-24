584178b6e94de517e64a4495201ee1911ea4f863
OlenaChemerynska2003 <chemerynska.olena@gmail.com>
2023-03-24 20:22:18 +0200

    TRTRfgfg

--- src/lab_1/Task2.java
+++ src/lab_1/Task2.java
@@ -26,9 +26,10 @@ public class Task2 {
         this.word10=word10;
         this.word11=word11;
         this.word12=word12;
+
     }
     public String concatWord (){
         String result = word1 + " " + word2 + " " + word3 + " " + word4 + " " + word5 + " " + word6 + " " + word7 + " " + word8 + " " + word9 + " " + word10 + " " + word11 + " " + word12;
-        return result;
+        return result;//grgtgtg
     }
 }
