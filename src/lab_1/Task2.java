5b6e279c2fe0853951eb5c68fcaa5c341f5586ff
OlenaChemerynska2003 <chemerynska.olena@gmail.com>
2023-03-24 20:24:54 +0200

    test commit

--- src/lab_1/Task2.java
+++ src/lab_1/Task2.java
@@ -30,6 +30,6 @@ public class Task2 {
     }
     public String concatWord (){
         String result = word1 + " " + word2 + " " + word3 + " " + word4 + " " + word5 + " " + word6 + " " + word7 + " " + word8 + " " + word9 + " " + word10 + " " + word11 + " " + word12;
-        return result;//grgtgtg
+        return result;
     }
 }
