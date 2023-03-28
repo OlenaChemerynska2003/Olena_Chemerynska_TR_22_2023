49931be8d0d1de001e1783f472b2f97ec8f5bf5e
OlenaChemerynska2003 <chemerynska.olena@gmail.com>
2023-03-28 19:51:22 +0300

    commit lab_2 version 0.1

---
+++ src/lab_2/Main.java
@@ -0,0 +1,47 @@
+package lab_2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("---------------------------TASK-1--------------------------------------");
+
+        Task1 task1 = new Task1(6, 8, 2);
+        System.out.println(task1.main_task1());
+
+        System.out.println("---------------------------TASK-1--------------------------------------");
+
+        System.out.println("---------------------------TASK-2--------------------------------------");
+
+        Task2 task2 = new Task2(3, 4);
+        System.out.println(task2.main_task2());
+
+        System.out.println("---------------------------TASK-2--------------------------------------");
+
+        System.out.println("---------------------------TASK-3--------------------------------------");
+
+        Task3 task3 = new Task3(6);
+        System.out.println(task3.main_task3_1());
+        System.out.println(task3.main_task3_2());
+
+        System.out.println("---------------------------TASK-3--------------------------------------");
+
+        System.out.println("---------------------------TASK-4--------------------------------------");
+
+        Task4 task4 = new Task4(3);
+        System.out.println(task4.main_task4());
+
+        System.out.println("---------------------------TASK-4--------------------------------------");
+
+        System.out.println("---------------------------TASK-5--------------------------------------");
+
+        Task5 task5 = new Task5(3,12,3);
+        System.out.println(task5.main_task5_1());
+        System.out.println(task5.main_task5_2());
+        System.out.println(task5.main_task5_3());
+        System.out.println(task5.main_task5_4());
+        System.out.println(task5.main_task5_5());
+        System.out.println(task5.main_task5_6());
+
+        System.out.println("---------------------------TASK-5--------------------------------------");
+    }
+
+}
